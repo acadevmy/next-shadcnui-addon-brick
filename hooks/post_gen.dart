d7adc3f177b7ba7e1c5c93fd9b4ac0ecf8127eaa
erikamolino <erikamolino15@gmail.com>
2024-05-10 10:49:28 +0200

    feat: shacnui config

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -1,5 +1,23 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {
-  // TODO: add post-generation logic.
+void run(HookContext context) async {
+  await runPnpm(context: context);
+}
+
+Future<void> runPnpm({
+  required HookContext context,
+}) async {
+  context.logger.info('📦 Running pnpm i');
+  await Process.run('pnpm', [
+    'i',
+    'class-variance-authority',
+    'clsx',
+    'lucide-react',
+    'tailwind-merge',
+    'tailwindcss-animate',
+    '@radix-ui/react-slot'
+  ]);
+  context.logger.success('📦 pnpm configured successfully 🚀');
 }
