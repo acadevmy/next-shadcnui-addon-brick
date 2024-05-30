c0dbcd94243db0ffa6513b09d5860d987359a384
Gabriele Bilello <gabriele.bilello1@gmail.com>
2024-05-30 11:10:20 +0200

    feat: moved postgen scripts to pregen

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -1,23 +1,3 @@
-import 'dart:io';
-
 import 'package:mason/mason.dart';
 
-void run(HookContext context) async {
-  await runPnpm(context: context);
-}
-
-Future<void> runPnpm({
-  required HookContext context,
-}) async {
-  context.logger.info('📦 Running pnpm i');
-  await Process.run('pnpm', [
-    'i',
-    'class-variance-authority',
-    'clsx',
-    'lucide-react',
-    'tailwind-merge',
-    'tailwindcss-animate',
-    '@radix-ui/react-slot'
-  ]);
-  context.logger.success('📦 pnpm configured successfully 🚀');
-}
+void run(HookContext context) async {}
--- hooks/pre_gen.dart
+++ hooks/pre_gen.dart
@@ -1,5 +1,22 @@
+import 'dart:io';
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {
-  // TODO: add pre-generation logic.
+Future<void> run(HookContext context) async {
+  await runPnpm(context: context);
+}
+
+Future<void> runPnpm({
+  required HookContext context,
+}) async {
+  context.logger.info('📦 Installing shadcn/ui dependencies');
+  await Process.run('pnpm', [
+    'i',
+    'class-variance-authority',
+    'clsx',
+    'lucide-react',
+    'tailwind-merge',
+    'tailwindcss-animate',
+    '@radix-ui/react-slot'
+  ]);
+  context.logger.success('📦 shadcn/ui configured successfully 🚀');
 }
